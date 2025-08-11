f07432843da92f6d4ae0af2b5404681e9f5108be
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-11 15:17:26 +0530

    settings.py

--- bluezqr/bluezqr/settings.py
+++ bluezqr/bluezqr/settings.py
@@ -79,13 +79,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bluezqr.wsgi.application'
 
-import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# import dj_database_url
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+#     )
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
