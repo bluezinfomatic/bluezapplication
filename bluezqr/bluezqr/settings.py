1ff49e593e95a8358c9d5eb382c9e74015feb3c4
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-11 16:32:07 +0530

    settings.py

--- bluezqr/bluezqr/settings.py
+++ bluezqr/bluezqr/settings.py
@@ -80,23 +80,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bluezqr.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# import dj_database_url
-# import os
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 
