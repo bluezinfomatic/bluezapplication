238ef592c383b4f0a31b7bab589fc725e586d95d
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-11 11:46:18 +0530

    views.py

--- bluezqr/users/views.py
+++ bluezqr/users/views.py
@@ -3,6 +3,10 @@ from .models import Student, Candidate
 from .serializers import StudentSerializer, CandidateSerializer
 from rest_framework.views import exception_handler
 
+# Extra imports for backup
+from django.core.management import call_command
+from django.http import HttpResponse, HttpResponseForbidden
+from io import StringIO
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -11,3 +15,15 @@ class StudentViewSet(viewsets.ModelViewSet):
 class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
+
+# ---- Temporary backup view ----
+def backup_view(request):
+    # Optional: restrict to admin users only
+    # if not request.user.is_authenticated or not request.user.is_staff:
+    #     return HttpResponseForbidden("Forbidden")
+
+    out = StringIO()
+    call_command('dumpdata', stdout=out)
+    response = HttpResponse(out.getvalue(), content_type='application/json')
+    response['Content-Disposition'] = 'attachment; filename=backup.json'
+    return response
