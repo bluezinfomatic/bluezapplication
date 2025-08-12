e1038f7cb7e28ee5e9f45339f38c35c0f4774e97
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-12 13:00:21 +0530

    serializers.py

--- bluezqr/users/serializers.py
+++ bluezqr/users/serializers.py
@@ -2,11 +2,25 @@ from rest_framework import serializers
 from .models import Student, Candidate
 
 class StudentSerializer(serializers.ModelSerializer):
+    resume_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = '__all__'  # or list all fields explicitly + 'resume_url'
+
+    def get_resume_url(self, obj):
+        if obj.resume:
+            return obj.resume.url
+        return None
 
 class CandidateSerializer(serializers.ModelSerializer):
+    resume_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Candidate
-        fields = '__all__'
+        fields = '__all__'  # or list all fields explicitly + 'resume_url'
+
+    def get_resume_url(self, obj):
+        if obj.resume:
+            return obj.resume.url
+        return None
