c049f07534fc03a3b2952cd601c139c58f49ac09
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-12 16:39:48 +0530

    views.py

--- bluezqr/users/views.py
+++ bluezqr/users/views.py
@@ -40,24 +40,4 @@ def create_admin_user():
 
 create_admin_user()
 
-# from cloudinary.utils import cloudinary_url
-# from django.shortcuts import redirect
-# from django.http import HttpResponse
-# from .models import Student
-
-# def download_resume(request, student_id):
-#     try:
-#         student = Student.objects.get(id=student_id)
-#         if student.resume:
-#             # Generate the proper Cloudinary URL for raw file (pdf)
-#             url, options = cloudinary_url(
-#                 student.resume.public_id,
-#                 resource_type="raw",
-#                 format="pdf"
-#             )
-#             return redirect(url)
-#         else:
-#             return HttpResponse("No resume uploaded", status=404)
-#     except Student.DoesNotExist:
-#         return HttpResponse("Student not found", status=404)
 
