2c9082cb61a041a8df623caa4008f0a5e8096178
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-14 10:34:07 +0530

    models.py

--- bluezqr/users/models.py
+++ bluezqr/users/models.py
@@ -12,7 +12,7 @@ class Student(models.Model):
     city = models.CharField(max_length=100)
     passout_year = models.IntegerField()
     percentage = models.FloatField()
-    resume = models.FileField(upload_to='resumes/students/')
+    resume = CloudinaryField('resume', resource_type='raw', folder='resumes/students')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -31,7 +31,7 @@ class Candidate(models.Model):
     passout_year = models.IntegerField()
     experience_years = models.IntegerField()
     experience_details = models.TextField()
-    resume = models.FileField(upload_to='resumes/candidates/')
+    resume = CloudinaryField('resume', resource_type='raw', folder='resumes/candidates')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
