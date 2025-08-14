cfa8b42beaf16a5360ee54b05e884835cf782ebd
bluezinfomatic <153344893+bluezinfomatic@users.noreply.github.com>
2025-08-14 12:52:53 +0530

    models.py

--- bluezqr/users/models.py
+++ bluezqr/users/models.py
@@ -1,38 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=15)
-    alternate_mobile = models.CharField(max_length=15, blank=True, null=True)
-    degree = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    college_name = models.CharField(max_length=200)
-    area = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    passout_year = models.IntegerField()
-    percentage = models.FloatField()
-    resume = CloudinaryField('resume', resource_type='raw')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Candidate(models.Model):
-    name = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=15)
-    alternate_mobile = models.CharField(max_length=15, blank=True, null=True)
-    branch = models.CharField(max_length=100)
-    degree = models.CharField(max_length=100)
-    department = models.CharField(max_length=100)
-    area = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    passout_year = models.IntegerField()
-    experience_years = models.IntegerField()
-    experience_details = models.TextField()
-    from django.db import models
-from cloudinary.models import CloudinaryField
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
@@ -45,7 +13,7 @@ class Student(models.Model):
     city = models.CharField(max_length=100)
     passout_year = models.IntegerField()
     percentage = models.FloatField()
-    resume = CloudinaryField('resume', resource_type='raw')
+    # Resume stored as raw file in Cloudinary folder "resumes/students"
     resume = CloudinaryField('resume', resource_type='raw', folder='resumes/students')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -65,11 +33,7 @@ class Candidate(models.Model):
     passout_year = models.IntegerField()
     experience_years = models.IntegerField()
     experience_details = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
+    # Resume stored as raw file in Cloudinary folder "resumes/candidates"
     resume = CloudinaryField('resume', resource_type='raw', folder='resumes/candidates')
     created_at = models.DateTimeField(auto_now_add=True)
 
